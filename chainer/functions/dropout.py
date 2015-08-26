b8b3091a1f4006282505307359abf26ff6b45cb1
woodshop <sarroff@cs.dartmouth.edu>
2015-08-26 19:07:40 -0400

    complex lstm and dropout

--- chainer/functions/dropout.py
+++ chainer/functions/dropout.py
@@ -68,3 +68,71 @@ def dropout(x, ratio=.5, train=True):
     if train:
         return Dropout(ratio)(x)
     return x
+
+
+class CplxDropout(Dropout):
+
+    """Dropout regularization."""
+
+    def check_type_forwrad(self, in_types):
+        type_check.expect(in_types.size() == 1)
+        type_check.expect(in_types[0].dtype == numpy.complex64)
+
+    def forward_cpu(self, x):
+        scale = numpy.float64(1. / (1 - self.dropout_ratio))
+        self.mask = scale * \
+            (numpy.random.rand(*x[0].shape) >= self.dropout_ratio)
+        return x[0] * self.mask,
+
+    def forward_gpu(self, x):
+        self.rand = cuda.empty(x[0].shape, dtype=numpy.float32)
+        y = cuda.empty_like(x[0])
+
+        cuda.get_generator().fill_uniform(self.rand)
+        self.scale = numpy.complex64(1. / (1 - self.dropout_ratio))
+
+        self.kernel = cuda.elementwise(
+            '''
+               pycuda::complex<float>* y,
+               const pycuda::complex<float>* x,
+               const float* rand,
+               float dropout_ratio,
+               pycuda::complex<float> scale
+            ''',
+            '''y[i] = rand[i] < dropout_ratio ? 
+                        pycuda::complex<float>(0) : scale * x[i]''',
+            'dropout')
+        self.kernel(y, x[0], self.rand, self.dropout_ratio, self.scale)
+        return y,
+
+    def backward_cpu(self, x, gy):
+        return gy[0] * self.mask,
+
+    def backward_gpu(self, x, gy):
+        gx = cuda.empty_like(gy[0])
+        self.kernel(gx, gy[0], self.rand, self.dropout_ratio, self.scale)
+        return gx,
+
+    
+def cplx_dropout(x, ratio=.5, train=True):
+    """Drops elements of input variable randomly.
+
+    This function drops input elements randomly with probability ``ratio`` and
+    scales the remaining elements by factor ``1 / (1 - ratio)``. In testing
+    mode, it does nothing and just returns ``x``.
+
+    Args:
+        x (~chainer.Variable): Input variable.
+        ratio (float): Dropout ratio.
+        train (bool): If True, executes dropout. Otherwise, does nothing.
+
+    Returns:
+        ~chainer.Variable: Output variable.
+
+    See the paper by G. Hinton: `Improving neural networks by preventing \
+    co-adaptation of feature detectors <http://arxiv.org/abs/1207.0580>`_.
+
+    """
+    if train:
+        return CplxDropout(ratio)(x)
+    return x
--- chainer/functions/lstm.py
+++ chainer/functions/lstm.py
@@ -226,3 +226,275 @@ def lstm(c_prev, x):
 
     """
     return LSTM()(c_prev, x)
+
+
+def _georgiou(x, c=1., r=1.):
+    return x / (c + 1./r * numpy.abs(x))
+
+
+def _grad_georgiou(x, c=1., r=1.):
+    x = numpy.real(x)
+    y = numpy.imag(x)
+    z = numpy.abs(z)
+    u_x = r*(y**2 + c*r*z) / (z*(c*r+z)**2)
+    u_y = - r*x*y / (z*(c*r+z)**2)
+    return u_x - 1j*u_y
+
+
+_cplx_preamble = '''
+using namespace pycuda;
+
+__device__ complex<float> georgiou(complex<float> x, float a0, float a1) {{ 
+    return x / (complex<float>(a0) + complex<float>(1)/complex<float>(a1) * 
+        abs(x));
+}}
+
+__device__ complex<float> grad_georgiou(complex<float> y, float a0, 
+                                        float a1) {{
+    float re = real(y);
+    float im = imag(y);
+    float z = abs(y);
+    float u_x = (z == 0) ? 
+        1 / a0 : a1*(im*im + a0*a1*z) / (z*(a0*a1+z)*(a0*a1+z));
+    float u_y = (z == 0) ? 
+        0 : - a1*re*im / (z*(a0*a1+z)*(a0*a1+z));
+    //float v_x = (z == 0) ? 
+    //    0 : - a1*re*im / (z*(a0*a1+z)*(a0*a1+z));
+    //float v_y = (z == 0) ? 
+    //    1 / a0 : a1*(re*re + a0*a1*z) / (z*(a0*a1+z)*(a0*a1+z));
+    return conj(complex<float>(u_x, u_y));
+}}
+
+__device__ complex<float> grad_tanh(complex<float> y) {{
+   return complex<float>(1) - y * y;
+
+}}
+
+#define COMMON_ROUTINE \
+    int I = i / rsize; \
+    int J = i % rsize; \
+    float a0 = {a0}; \
+    float a1 = {a1}; \
+    const complex<float>* x_i = x + I * 4 * rsize; \
+    complex<float> aa =     tanh(x_i[          J]); \
+    complex<float> ai = georgiou(x_i[  rsize + J], a0, a1); \
+    complex<float> af = georgiou(x_i[2*rsize + J], a0, a1); \
+    complex<float> ao = georgiou(x_i[3*rsize + J], a0, a1);
+'''
+
+class CplxLSTM(function.Function):
+
+    """Long short-term memory unit with forget gate.
+
+    It has two inputs (c, x) and two outputs (c, h), where c indicates the cell
+    state. x must have four times channels compared to the number of units.
+
+    """
+
+    def __init__(self, a0=1, a1=1):
+        self.a0 = a0
+        self.a1 = a1
+        super(function.Function, self).__init__()
+    
+    def check_type_forward(self, in_types):
+        type_check.expect(in_types.size() == 2)
+        c_type, x_type = in_types
+
+        type_check.expect(
+            c_type.dtype == numpy.complex64,
+            x_type.dtype == numpy.complex64,
+
+            c_type.ndim >= 2,
+            x_type.ndim >= 2,
+            c_type.ndim == x_type.ndim,
+
+            x_type.shape[0] == c_type.shape[0],
+            x_type.shape[1] == 4 * c_type.shape[1],
+        )
+        for i in range(2, c_type.ndim.eval()):
+            type_check.expect(x_type.shape[i] == c_type.shape[i])
+
+    def forward_cpu(self, inputs):
+        c_prev, x = inputs
+
+        a, i, f, o = _extract_gates(x)
+        self.a = numpy.tanh(a)
+        self.i = _georgiou(i)
+        self.f = _georgiou(f)
+        self.o = _georgiou(o)
+
+        self.c = self.a * self.i + self.f * c_prev
+        h = self.o * numpy.tanh(self.c)
+        return self.c, h
+
+    def backward_cpu(self, inputs, grad_outputs):
+        c_prev = inputs[0]
+        gc, gh = grad_outputs
+
+        gx = numpy.empty_like(inputs[1])
+        ga, gi, gf, go = _extract_gates(gx)
+
+        # Consider the case that either gradient is not given
+        if gc is None:
+            gc = 0
+        if gh is None:
+            gh = 0
+
+        co = numpy.tanh(self.c)
+        gc_prev = gh * self.o * _grad_tanh(co) + gc  # multiply f later
+        ga[:] = gc_prev * self.i * _grad_tanh(self.a)
+        gi[:] = gc_prev * self.a * _grad_georgiou(self.i)
+        gf[:] = gc_prev * c_prev * _grad_georgiou(self.f)
+        go[:] = gh * co * _grad_georgiou(self.o)
+        gc_prev *= self.f  # multiply f here
+
+        return gc_prev, gx
+
+    def forward_gpu(self, inputs):
+        c_prev, x = inputs
+        lsize = c_prev.shape[0] * c_prev.shape[1]
+        rsize = c_prev.size // lsize
+        a0 = cuda.to_gpu(numpy.array(self.a0))
+        a1 = cuda.to_gpu(numpy.array(self.a1))
+        self.c = cuda.empty_like(c_prev)
+        h = cuda.empty_like(c_prev)
+        cuda.elementwise(
+            '''pycuda::complex<float>* c, 
+               pycuda::complex<float>* h, 
+               const pycuda::complex<float>* c_prev, 
+               const pycuda::complex<float>* x,
+               const float* a0,
+               const float* a1,
+               int lsize,
+               int rsize''',
+            '''COMMON_ROUTINE;
+               c[i] = aa * ai + af * c_prev[i];
+               h[i] = ao * tanh(c[i]);''',
+            'lstm_fwd',
+            preamble=_cplx_preamble.format(a0=self.a0, a1=self.a1)
+        )(self.c, h, c_prev, x, a0, a1, lsize, rsize)
+
+        return self.c, h
+
+    def backward_gpu(self, inputs, grad_outputs):
+        c_prev, x = inputs
+        gc, gh = grad_outputs
+        lsize = c_prev.shape[0] * c_prev.shape[1]
+        rsize = c_prev.size // lsize
+        a0 = cuda.to_gpu(numpy.array(self.a0))
+        a1 = cuda.to_gpu(numpy.array(self.a1))
+
+        # Odd rule to determine whether the gradient is given or not.
+        if gc is None:
+            gc = self.c
+        if gh is None:
+            gh = self.c
+
+        gc_prev = cuda.empty_like(c_prev)
+        gx = cuda.empty_like(x)
+        cuda.elementwise(
+            '''
+               pycuda::complex<float>* gc_prev, 
+               pycuda::complex<float>* gx, 
+               const pycuda::complex<float>* c_prev, 
+               const pycuda::complex<float>* x,
+               const float* a0,
+               const float* a1,
+               const pycuda::complex<float>* c, 
+               const pycuda::complex<float>* gc,
+               const pycuda::complex<float>* gh, 
+               int lsize,
+               int rsize
+            ''', '''
+               COMMON_ROUTINE;
+               complex<float>* gx_i = gx + I * 4 * rsize;
+               complex<float>& ga = gx_i[          J];
+               complex<float>& gi = gx_i[  rsize + J];
+               complex<float>& gf = gx_i[2*rsize + J];
+               complex<float>& go = gx_i[3*rsize + J];
+
+               complex<float> co  = tanh(c[i]);
+               // Odd rule: if gh == c [gc == c] then gh [gc] is not given,
+               // since we cannot pass null pointer to the kernel through
+               // PyCUDA.
+               complex<float> gc1 = (gh == c ? complex<float>(0) : 
+                         gh[i] * ao * grad_tanh(co))
+                         + (gc == c ? complex<float>(0) : gc[i]);
+               go        =  gh == c ? complex<float>(0) : gh[i] * co * 
+                                     grad_georgiou(ao, {a0}, {a1});
+
+               gc_prev[i] = gc1 * af;
+               ga         = gc1 * ai        * grad_tanh(aa);
+               gi         = gc1 * aa        * grad_georgiou(ai, {a0}, {a1});
+               gf         = gc1 * c_prev[i] * grad_georgiou(af, {a0}, {a1});
+            '''.format(a0=self.a0, a1=self.a1),
+            'lstm_bwd',
+            preamble=_cplx_preamble.format(a0=self.a0, a1=self.a1))(
+                gc_prev, gx, c_prev, x, a0, a1, self.c, gc, gh,
+                lsize, rsize)
+
+        return gc_prev, gx
+
+
+# def cplx_lstm(c_prev, x):
+#     """Long Short-Term Memory units as an activation function.
+
+#     This function implements LSTM units with forget gates. Let the previous
+#     cell state :math:`c_{\\text{prev}}` and the incoming signal :math:`x`.
+
+#     First, the incoming signal :math:`x` is split into four arrays
+#     :math:`a, i, f, o` of the same shapes along the second axis.
+#     It means that :math:`x` 's second axis must have 4 times the length of
+#     :math:`c_{\\text{prev}}`.
+
+#     The splitted input signals are corresponding to:
+
+#         - :math:`a` : sources of cell input
+#         - :math:`i` : sources of input gate
+#         - :math:`f` : sources of forget gate
+#         - :math:`o` : sources of output gate
+
+#     Second, it computes outputs as:
+
+#     .. math::
+
+#         c &= \\tanh(a) \\text{sigmoid}(i)
+#            + c_{\\text{prev}} \\text{sigmoid}(f), \\\\
+#         h &= \\tanh(c) \\text{sigmoid}(o).
+
+#     These are returned as a tuple of two variables.
+
+#     Args:
+#         c_prev (~chainer.Variable): Variable that holds the previous cell
+#             state. The cell state should be a zero array or the output of the
+#             previous call of LSTM.
+#         x (~chainer.Variable): Variable that holds the incoming signal. It must
+#             have the second dimension four times of that of the cell state,
+
+#     Returns:
+#         tuple: Two :class:`~chainer.Variable` objects ``c`` and ``h``. ``c`` is
+#             the updated cell state. ``h`` indicates the outgoing signal.
+
+#     See the original paper proposing LSTM with forget gates:
+#     `Long Short-Term Memory in Recurrent Neural Networks \
+#     <http://www.felixgers.de/papers/phd.pdf>`_.
+
+#     .. admonition:: Example
+
+#         Assuming ``y`` is the current input signal, ``c`` is the previous cell
+#         state, and ``h`` is the previous output signal from an ``lstm``
+#         function. Each of ``y``, ``c`` and ``h`` has ``n_units`` channels.
+#         Most typical preparation of ``x`` is:
+
+#         >>> model = FunctionSet(w=F.Linear(n_units, 4 * n_units),
+#         ...                     v=F.Linear(n_units, 4 * n_units),
+#         ...                     ...)
+#         >>> x = model.w(y) + model.v(h)
+#         >>> c, h = F.lstm(c, x)
+
+#         It corresponds to calculate the input sources :math:`a, i, f, o` from
+#         the current input ``y`` and the previous output ``h``. Different
+#         parameters are used for different kind of input sources.
+
+#     """
+#     return CplxLSTM()(c_prev, x)
