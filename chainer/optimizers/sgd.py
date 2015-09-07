extensive updates to (hopefully) correct (conjugate) gradients

chainer/optimizers/sgd.py
@@ -1,6 +1,6 @@
 from chainer import cuda
 from chainer import optimizer
-
+import numpy as np
 
 class SGD(optimizer.Optimizer):
 
@@ -13,6 +13,20 @@ class SGD(optimizer.Optimizer):
         param -= self.lr * grad
 
     def update_one_gpu(self, param, grad, _):
+        assert param.dtype == np.float32
+        assert grad.dtype == np.float32
         cuda.elementwise('float* param, const float* grad, float lr',
                          'param[i] -= lr * grad[i]',
                          'sgd')(param, grad, self.lr)
+
+class CplxSGD(SGD):
+
+    def update_one_gpu(self, param, grad, _):
+        cuda.elementwise(
+            '''
+               pycuda::complex<float>* param, 
+               const pycuda::complex<float>* grad,
+               float lr
+            ''',
+            'param[i] -= lr * grad[i]',
+            'sgd')(param, grad, self.lr)